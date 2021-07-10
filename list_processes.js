9a61440617399bcdee248954534a3dd48f8cca7c
rootVIII <colleyloyejames@gmail.com>
2021-07-10 15:59:26 -0400

    return processes in object

--- list_processes.js
+++ list_processes.js
@@ -3,6 +3,7 @@ const { execSync } = require('child_process');
 class PSCmd {
     constructor() {
         this.cmd = 'ps -ec -o pid -o %cpu -o %mem -o command';
+        this.processes = {};
     }
 
     * execPS() {
@@ -22,14 +23,29 @@ class PSCmd {
 
     run() {
         for (let process of this.filterStdout()) {
-            console.log(process);
+            let currentPS = process[process.length - 1];
+            // console.log(currentPS);
+            if (!(currentPS in this.processes)) {
+                this.processes[currentPS] = [process.slice(0, 3)];
+            } else {
+                this.processes[currentPS].push(process.slice(0, 3));
+            }
         }
+    }
+
+    getProcessDetails() {
+        return this.processes;
     }
 }
 
 function main() {
     let ps = new PSCmd();
     ps.run();
+    Object.entries(ps.getProcessDetails()).forEach(([key, val]) => {
+        console.log(key);
+        console.log(val);
+        console.log('\n');
+    });
 }
 
 main();
