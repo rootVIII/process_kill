88323c0b113ac473f9ed5740f3f202228571997b
rootVIII <colleyloyejames@gmail.com>
2021-07-10 15:21:23 -0400

    package.json

--- list_processes.js
+++ list_processes.js
@@ -2,23 +2,28 @@ const { execSync } = require('child_process');
 
 class PSCmd {
     constructor() {
-        this.cmd = 'ps -e -o pid -o %cpu -o %mem -o command';
+        this.cmd = 'ps -ec -o pid -o %cpu -o %mem -o command';
     }
 
-    * runPS() {
-        for (const val of execSync(this.cmd).toString().split('\n')) {
-            yield val.trimLeft();
+    * execPS() {
+        for (const psLine of execSync(this.cmd).toString().split('\n')) {
+            if (psLine.length > 0) {
+                yield psLine.trimLeft();
+            }
         }
-        // execSync(this.cmd).toString().split('\n').forEach((val) => {
-        // console.log(val.trimLeft());
-        // yield val.trimLeft();
-        // });
+    }
+
+    * run() {
+        for (const trimmedLine of this.execPS()) {
+            const ps = trimmedLine.split(/[ ]+/);
+            yield ps.slice(0, 3).concat(ps.slice(3, ps.length).join(' '));
+        }
     }
 }
 
 function main() {
     let ps = new PSCmd();
-    for (let process of ps.runPS()) {
+    for (let process of ps.run()) {
         console.log(process);
     }
 }
